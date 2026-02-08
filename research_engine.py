f7fcb345c6045654067090997619e294db4e16df
Ygtopher <nsechris0@gmail.com>
2026-02-08 13:28:00 +0200

    Add dropout layers to prevent overfitting (v863)

--- research_engine.py
+++ research_engine.py
@@ -3452,3 +3452,7 @@ vocab_size = 32000
         scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.d_model)
     loss = calculate_loss(outputs, labels)
 import math
+        return F.softmax(scores, dim=-1)
+vocab_size = 32000
+hidden_dim = 768
+scaler = torch.cuda.amp.GradScaler()
